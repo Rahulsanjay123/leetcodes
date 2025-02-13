cde1b59eb8e47e3e9e5c8bab3cc03c5728c8f63a
Rahulsanjay123 <rahulsanjay1124@gmail.com>
2025-02-13 14:59:52 +0530

    Time: 4 ms (2.68%), Space: 8.1 MB (95.55%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.c
@@ -0,0 +1,12 @@
+char* longestCommonPrefix(char** strs, int strsSize) {
+    if (strsSize == 0) return "";
+    char* prefix = strs[0];
+    for (int i = 1; i < strsSize; i++) {
+        while (strncmp(prefix, strs[i], strlen(prefix)) != 0) {
+            prefix[strlen(prefix) - 1] = '\0';
+        }
+    }
+    return prefix;
+
+    
+}
