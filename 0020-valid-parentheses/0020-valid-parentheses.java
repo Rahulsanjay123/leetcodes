807e9d44817a29026215581156465644b5609067
Rahulsanjay123 <rahulsanjay1124@gmail.com>
2025-02-13 15:00:20 +0530

    Time: 0 ms (100%), Space: 41.6 MB (81.18%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean isValid(String s) {
+        char[] stack = new char[s.length()];
+        int top = -1;
+
+        for (char ch : s.toCharArray()) {
+            switch (ch) {
+                case '(':
+                case '[':
+                case '{':
+                    stack[++top] = ch;
+                    break;
+                case ')':
+                    if (top == -1 || stack[top--] != '(') {
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (top == -1 || stack[top--] != '[') {
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (top == -1 || stack[top--] != '{') {
+                        return false;
+                    }
+                    break;
+                default:
+                    
+                    break;
+            }
+        }
+
+        return top == -1;
+    }
+};
