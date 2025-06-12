a9723461118fef161c434290cc2966dc40870a4a
Rahulsanjay123 <rahulsanjay1124@gmail.com>
2025-06-12 10:23:43 +0530

    Time: 0 ms (100%), Space: 40.7 MB (21.12%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,21 +1,5 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        if(num==1)
-        {
-            return false;
-        }
-        return num==div(num);
-    }
-    public static int div(int a)
-    {
-        int sum=1;
-        for(int i=2;i<=a/2;i++)
-        {
-            if(a%i==0)
-            {
-                sum+=i;
-            }
-        }
-        return sum;
+        return (num==6 || num==28 || num==496 || num==8128 || num==33550336);
     }
 }
