136f8474c4e882a139fdb64ca25ce8a8208f2d7b
Rahulsanjay123 <rahulsanjay1124@gmail.com>
2025-06-19 15:43:18 +0530

    Time: 0 ms (100%), Space: 41.8 MB (30.28%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.java
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -1,11 +1,12 @@
 class Solution {
     public String toLowerCase(String s) {
-        char ch[]=s.toCharArray();
-        for(int i=0;i<ch.length;i++)
-        {
-            if(ch[i]>='A' && ch[i]<='Z')
-            {ch[i]+=32;}
-        }
-        return new String(ch);
+        // char ch[]=s.toCharArray();
+        // for(int i=0;i<ch.length;i++)
+        // {
+        //     if(ch[i]>='A' && ch[i]<='Z')
+        //     {ch[i]+=32;}
+        // }
+        // return new String(ch);
+        return s.toLowerCase();
     }
 }
