efc53f008d66556080b722f61bad913ca6c1079e
Rahulsanjay123 <rahulsanjay1124@gmail.com>
2025-06-24 11:50:05 +0530

    Time: 0 ms (100%), Space: 40.2 MB (91.42%) - LeetHub

--- 2491-smallest-even-multiple/2491-smallest-even-multiple.java
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.java
@@ -1,14 +1,12 @@
 class Solution {
     public int smallestEvenMultiple(int n) {
-        int ans=0;
         if(n%2==0)
         {
-            ans=n;
+            return n;
         }
         else
         {
-            ans=n*2;
+            return n*2;
         }
-        return ans;
     }
 }
