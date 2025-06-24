f499a4c99483644afeb3144d3ed1dd9bdf26b21d
Rahulsanjay123 <rahulsanjay1124@gmail.com>
2025-06-24 11:42:52 +0530

    Time: 0 ms (100%), Space: 40.5 MB (52.75%) - LeetHub

---
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int match=0;
+        while(n!=1)
+        {
+            match+=n/2;
+            n=n-(n/2);
+        }
+        return match;
+    }
+}
