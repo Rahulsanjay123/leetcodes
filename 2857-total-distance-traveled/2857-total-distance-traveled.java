fcc805e71fed0a30222cc6c3f9462e2e37873c9a
Rahulsanjay123 <rahulsanjay1124@gmail.com>
2025-06-25 09:47:51 +0530

    Time: 4 ms (99.74%), Space: 43.9 MB (90.65%) - LeetHub

--- 2857-total-distance-traveled/2857-total-distance-traveled.java
+++ 2857-total-distance-traveled/2857-total-distance-traveled.java
@@ -1,16 +1,6 @@
 class Solution {
-    public int distanceTraveled(int mainTank, int additionalTank) {
-        int distance = 0;
-        while (mainTank >= 5 && additionalTank > 0) 
-        {
-            int fuelUsed = 5;
-            mainTank -= fuelUsed;
-            mainTank += 1;
-            additionalTank--;
-            distance += fuelUsed * 10;
-        }
-        distance += mainTank * 10;
-        return distance;
-
+    public int distanceTraveled(int mainTank, int additionalTank)
+     {
+        return (mainTank + Math.min((mainTank-1)/4,additionalTank))*10;
     }
 }
