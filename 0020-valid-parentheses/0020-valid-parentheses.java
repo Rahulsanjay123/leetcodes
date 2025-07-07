3116817600dbe6f3c5a4dc820da3c0231dc2ec02
Rahulsanjay123 <rahulsanjay1124@gmail.com>
2025-07-07 14:24:26 +0530

    Time: 0 ms (100%), Space: 41.6 MB (81.82%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -4,33 +4,43 @@ class Solution {
         int top = -1;
 
         for (char ch : s.toCharArray()) {
-            switch (ch) {
+            switch (ch) 
+            {
                 case '(':
                 case '[':
                 case '{':
                     stack[++top] = ch;
                     break;
                 case ')':
-                    if (top == -1 || stack[top--] != '(') {
+                {
+                    if (top == -1 || stack[top--] != '(') 
+                    {
                         return false;
                     }
                     break;
+                }
                 case ']':
-                    if (top == -1 || stack[top--] != '[') {
+                {
+                    if (top == -1 || stack[top--] != '[') 
+                    {
                         return false;
                     }
                     break;
+                }
                 case '}':
-                    if (top == -1 || stack[top--] != '{') {
+                {
+                    if (top == -1 || stack[top--] != '{') 
+                    {
                         return false;
                     }
                     break;
+                }
                 default:
-                    
+                {
                     break;
+                }
             }
         }
-
         return top == -1;
     }
 };
