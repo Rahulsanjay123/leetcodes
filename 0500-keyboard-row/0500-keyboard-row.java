4761efd4f3e8ecf878782b624d1b9fe438d3128e
Rahulsanjay123 <rahulsanjay1124@gmail.com>
2026-03-05 15:54:45 +0530

    Time: 0 ms (100%), Space: 43.2 MB (15.63%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.java
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -1,33 +1,21 @@
 class Solution {
     public String[] findWords(String[] words) {
-        HashMap<Character,Integer>map=new HashMap<>();
-        String row1="qwertyuiop";
-        String row2="asdfghjkl";
-        String row3="zxcvbnm";
-        for(char ch:row1.toCharArray()){
-            map.put(ch,1);
+         ArrayList<String> ans=new ArrayList<>();
+       String first = "qwertyuiop";
+       String Secound ="asdfghjkl";
+       String Third="zxcvbnm";
+       for(String i : words){
+        if(isinrow(i,first) || isinrow(i,Secound) || isinrow(i,Third))
+         ans.add(i);
+       } 
+       return ans.toArray(new String[0]);
+    }
+    private boolean isinrow(String s,String row){
+      for(char c:s.toCharArray()){
+        if(row.indexOf(Character.toLowerCase(c))==-1){
+            return false;
         }
-        for(char ch:row2.toCharArray()){
-            map.put(ch,2);
-        }
-        for(char ch:row3.toCharArray()){
-            map.put(ch,3);
-        }
-        List<String> result=new ArrayList<>();
-        for(String word:words){
-            String lower=word.toLowerCase();
-            int firstrow=map.get(lower.charAt(0));
-            boolean valid=true;
-            for(char ch:lower.toCharArray()){
-                if(map.get(ch)!=firstrow){
-                    valid=false;
-                    break;
-                }
-            }
-            if(valid){
-                result.add(word);
-            }
-        }
-        return result.toArray(new String[0]);
+      }
+      return true;
     }
 }
